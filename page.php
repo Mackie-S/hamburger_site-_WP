dd2eaf59249f58dd752faa72a7cc40253abcff63
Mackie-s <masaki.shimadu@gmail.com>
2022-02-14 23:53:24 +0900

    singleとpageのループ処理テンプレートパーツであるloop-single作成

--- page.php
+++ page.php
@@ -5,23 +5,9 @@ Template Name: 私のカスタムページ
 ?>
 <?php get_header(); ?>
 <main class="l-main-wrapper">
-  <article class="p-single-main">
-    <?php
-if(have_posts()):
-	while(have_posts()):
-		the_post();?>
-    <div class="p-new-hero c-hero">
-      <?php the_post_thumbnail('full',array('class' =>'p-single-main__hero' )); ?>
-      <h1 class="p-new-hero__title"><?php the_title();?></h1>
-    </div>
-    <div class="c-container">
-      <article id="post-<?php the_ID(); ?>" <?php post_class('c-description'); ?>>
-        <?php the_content(); ?>
-    </div>
-    <?php endwhile;?>
-    <?php
-    endif; ?>
-  </article>
+  <!-- ループここから -->
+  <?php get_template_part( 'loop-single' ); ?>
+  <!-- ループここまで -->
 </main>
 <?php get_sidebar(); ?>
 <div class="c-cover"></div>
--- single.php
+++ single.php
@@ -1,25 +1,8 @@
 <?php get_header(); ?>
 <main class="l-main-wrapper">
-  <article class="p-single-main">
-    <?php
-    if( have_posts() ) :
-        while( have_posts() ) :
-            the_post(); ?>
-    <div class="p-new-hero c-hero">
-      <?php the_post_thumbnail('full',array('class' =>'p-single-main__hero' )); ?>
-      <h1 class="p-new-hero__title"><?php the_title();?></h1>
-    </div>
-
-    <div class="c-container">
-      <article id="post-<?php the_ID(); ?>" <?php post_class('c-description'); ?>>
-        <?php the_content(); ?>
-    </div>
-    <?php wp_link_pages(); ?>
-    <?php endwhile;
-    else :
-    ?><p>表示する記事がありません</p><?php
-    endif; ?>
-  </article>
+  <!-- ループここから -->
+  <?php get_template_part( 'loop-single' ); ?>
+  <!-- ループここまで -->
 </main>
 <?php get_sidebar(); ?>
 <div class="c-cover"></div>
